c0a8f993ea3cda77503a73c95a03a0c6889fcbee
suresh170411 <suresh.20fk@gmail.com>
2023-03-24 08:11:23 +0530

    DEPENDENCY : all jar files added_SpringBoot

---
+++ TaskPlannerProject/src/main/java/com/payPal/TaskPlannerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.payPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerProjectApplication.class, args);
+	}
+
+}
---
+++ TaskPlannerProject/src/test/java/com/payPal/TaskPlannerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
